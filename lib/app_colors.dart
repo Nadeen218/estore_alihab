2fb4d698b710ed4d17ee584a0163384a1b62a855
Nadeen218 <154513596+Nadeen218@users.noreply.github.com>
2026-07-16 22:18:09 +0300

    update on app colors

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const background = Color(0xFF0B0F1E);
-  static const backgroundSecondary = Color(0xFF12172A);
-  static const cardGradientStart = Color(0xFF3A5CE0);
-  static const cardGradientEnd = Color(0xFF1B2A6B);
-  static const accentGold = Color(0xFFFFB648);
-  static const accentPink = Color(0xFFFF6584);
-  static const accentCyan = Color(0xFF3CD9C7);
-  static const accentBlue = Color(0xFF4E8CFF);
-  static const statCardBg = Color(0xFFEFF1F8);
-  static const textDark = Color(0xFF1A1E2C);
-  static const textMuted = Color(0xFF8C93A8);
-  static const textLight = Color(0xFFFFFFFF);
-  static const navBg = Color(0xFFFFFFFF);
+  static const Color primaryBlue = Color(0xFF0052B4);
+  static const Color accentGreen = Color(0xFF5EC401);
+
+  static const Color darkBackground = Color(0xFF0B0F1E);
+  static const Color darkBackgroundSecondary = Color(0xFF12172A);
+  static const Color darkCardGradientStart = Color(0xFF3A5CE0);
+  static const Color darkCardGradientEnd = Color(0xFF1B2A6B);
+  static const Color darkTextLight = Color(0xFFFFFFFF);
+  static const Color darkTextMuted = Color(0xFF8C93A8);
+
+  static const Color lightBackground = Color(0xFFF8FAFC);
+  static const Color lightBackgroundSecondary = Color(0xFFFFFFFF);
+  static const Color lightCardGradientStart = Color(0xFF0052B4);
+  static const Color lightCardGradientEnd = Color(0xFF0B2C7A);
+  static const Color lightTextDark = Color(0xFF1A1E2C);
+  static const Color lightTextMuted = Color(0xFF707070);
+
+  static const Color accentGold = Color(0xFFFFB648);
+  static const Color accentPink = Color(0xFFFF6584);
+  static const Color accentCyan = Color(0xFF3CD9C7);
+  static const Color accentBlue = Color(0xFF4E8CFF);
 }
