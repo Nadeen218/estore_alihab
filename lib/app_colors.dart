a72edecfad90447460a88e0e9a3396dee37e16ef
Nadeen218 <154513596+Nadeen218@users.noreply.github.com>
2026-07-16 19:23:32 +0300

    Setting the main colors for the application

---
+++ lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF0052B4);
+  static const Color accentGreen = Color(0xFF5EC401);
+  static const Color backgroundLight = Color(0xFFF0F4F8);
+  static const Color cardWhite = Colors.white;
+}
